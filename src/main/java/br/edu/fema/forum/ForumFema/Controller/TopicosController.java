0d73581cc6bba191d13b8442bb121fd4f0fff4c0
gGalvão <114706794+GabrielGalvao012@users.noreply.github.com>
2024-03-04 20:54:07 -0300

    Forum Fema

---
+++ src/main/java/br/edu/fema/forum/ForumFema/Controller/TopicosController.java
@@ -0,0 +1,28 @@
+package br.edu.fema.forum.ForumFema.Controller;
+
+import br.edu.fema.forum.ForumFema.model.Curso;
+import br.edu.fema.forum.ForumFema.model.DTO.TopicosDTO;
+import br.edu.fema.forum.ForumFema.model.Topico;
+import br.edu.fema.forum.ForumFema.repository.TopicosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicosController {
+
+    @Autowired
+    private TopicosRepository topicosRepository;
+
+    @RequestMapping("/topicos")
+    public List<TopicosDTO> lista(){
+
+            List<Topico> topicos = topicosRepository.findAll();
+        return TopicosDTO.converter(topicos);
+    }
+
+}
